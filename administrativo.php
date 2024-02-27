f01121767531093fff83082b308050d022940231
douglas <douglasalencarfixou78@gmail.com>
2024-02-27 18:31:43 -0300

    atualização e criação de menu lateral

--- administrativo.php
+++ administrativo.php
@@ -1,9 +1,102 @@
-<?php
+<?php 
 session_start();
-if(!empty($_SESSION['id'])){
-	echo "Olá ".$_SESSION['nome'].", Bem vindo <br>";
-	echo "<a href='sair.php'>Sair</a>";
-}else{
+if(empty($_SESSION['id'])){
 	$_SESSION['msg'] = "Área restrita";
 	header("Location: login.php");	
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css" rel="stylesheet" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<title>Document</title>
+
+</head>
+<body class="flex w-screen h-screen p-0 space-x-6 bg-gray-300">
+
+<div class="flex flex-col items-center w-16 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
+		<a class="flex items-center justify-center mt-3" href="#">
+			<svg class="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+			</svg>
+		</a>
+		<div class="flex flex-col items-center mt-3 border-t border-gray-700">
+			<a class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				 	<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+				</svg>
+			</a>
+			<a class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+				</svg>
+			</a>
+			<a class="flex items-center justify-center w-12 h-12 mt-2 text-gray-200 bg-gray-700 rounded" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+				</svg>
+			</a>
+			<a class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+				</svg>
+			</a>
+		</div>
+		<div class="flex flex-col items-center mt-2 border-t border-gray-700">
+			<a class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+				</svg>
+			</a>
+			<a class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+				</svg>
+			</a>
+			<a class="relative flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
+				<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+				</svg>
+				<span class="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
+			</a>
+		</div>
+		<button id="usurio" data-bs-toggle="modal" data-bs-target="#exampleModal" class="flex items-center justify-center w-16 h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300" href="#">
+			<svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+			</svg>
+		</button>
+	</div>
+	<!-- Component End  -->
+	
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Informações do usuário</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        <button id="deslogar" type="button" class="btn btn-primary">Deslogar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+<script>
+$(document).ready(function(){
+    $("#deslogar").click(function(){
+		window.location.href = "sair.php";// Substitua "pagina.html" pelo caminho da sua página
+    });
+});
+</script>
+</html>
--- login.php
+++ login.php
@@ -3,38 +3,58 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title>Celke - Login</title>
-	</head>
-	<body>
-		<h2>Área restrita</h2>
-		<?php
-			if(isset($_SESSION['msg'])){
-				echo $_SESSION['msg'];
-				unset($_SESSION['msg']);
-			}
-			if(isset($_SESSION['msgcad'])){
-				echo $_SESSION['msgcad'];
-				unset($_SESSION['msgcad']);
-			}
-		?>
-		<form method="POST" action="valida.php">
-			<label>Usuário</label>
-			<input type="text" name="usuario" placeholder="Digite o seu usuário"><br><br>
-			
-			<label>Senha</label>
-			<input type="password" name="senha" placeholder="Digite a sua senha"><br><br>
-			
-			<input type="submit" name="btnLogin" value="Acessar">
-			
-			<h4>Você ainda não possui uma conta?</h4>
-			<a href="cadastrar.php">Crie grátis</a>
-		
-		</form>
-		<br><br><br>
-		Usuário cadastrado no Banco de Dados<br>
-		Usuário: cesar@celke.com.br <br>
-		Senha: 123 <br>
-	</body>
+<head>
+    <meta charset="utf-8">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Celke - Login</title>
+    <style>
+        /* Adicionando estilo para centralizar o formulário */
+        .centered-form {
+            margin-top: 60px;
+        }
+    </style>
+</head>
+<body>
+<?php
+if(isset($_SESSION['msg'])){
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
+}
+if(isset($_SESSION['msgcad'])){
+    echo $_SESSION['msgcad'];
+    unset($_SESSION['msgcad']);
+}
+?>
+<form method="POST" action="valida.php">
+    <div class="container">
+        <div class="row justify-content-center centered-form">
+            <div class="col-md-4">
+                    <div class="card-body">
+                        <form action="" autocomplete="off">
+						<div class="main-login2 main-center">
+                            <div class="form-group">
+                                <label for="usuario" class="cols-sm-2 control-label">Usuário:</label>
+                                <input type="text" class="form-control" name="usuario" id="usuario" placeholder="Digite o seu usuário">
+                            </div>
+                            <div class="form-group">
+                                <label for="senha" class="cols-sm-2 control-label">Senha:</label>
+                               <input type="password" class="form-control" name="senha" id="senha" placeholder="Digite a sua senha">
+                            </div>
+                            
+                        </form>
+						 <input type="submit"  name="btnLogin" value="Acessar" class="btn">
+                         <br>  
+						 <div class="login-register-wrapper">
+   						 <h3 class="login-register">esqueceu sua senha? <a href="cadastrar.php"> resgatar</a></h3>
+						</div>
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+</form>
+</body>
 </html>
