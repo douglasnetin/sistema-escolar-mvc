e0d3917036f637ec1b3ad407723b8bed0bd20180
douglas <douglasalencarfixou78@gmail.com>
2024-03-03 00:10:18 -0300

    criação do painel de agendamento

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="login.php">portal</a>
-</body>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Navbar Animação em Bolha</title>
+    <link rel="stylesheet" href="/assets/css/HomePage.css" />
+    <script defer src="/assets/js/Home.js"></script>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="nav">
+        <a href="/" class="logo">🐅</a>
+        <button class="hamburger"></button>
+        <ul class="nav-list">
+          <li><a href="login.php">login</a></li>
+          <li><a href="cadastrar.php">cadastros</a></li>
+          <li><a href="log.php">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <main class="hero"></main>
+    <footer class="rodape">
+        <div>
+            <p>Entre em contato conosco:</p>
+            <ul>
+                <li>E-mail: exemplo@exemplo.com</li>
+                <li>Telefone: (XX) XXXX-XXXX</li>
+                <li>Endereço: 123 Rua Exemplo, Cidade, País</li>
+            </ul>
+        </div>
+    </footer>
+  </body>
 </html>
